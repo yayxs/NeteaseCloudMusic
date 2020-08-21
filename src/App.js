b19de608bba81bdc7cfd76d6d22c05fa10b2a1e2
yayxs <leaderywl@163.com>
2020-08-21 20:55:27 +0800

    feat:init pro

---
+++ src/App.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: yayxs
+ * @Date: 2020-08-21 20:44:48
+ * @LastEditTime: 2020-08-21 20:47:55
+ * @LastEditors: yayxs
+ * @Description:
+ * @FilePath: \NeteaseCloudMusic\src\App.js
+ * @
+ */
+import React from "react";
+
+function App() {
+  return <></>;
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,14 @@
+/*
+ * @Author: yayxs
+ * @Date: 2020-08-21 20:44:48
+ * @LastEditTime: 2020-08-21 20:47:34
+ * @LastEditors: yayxs
+ * @Description:
+ * @FilePath: \NeteaseCloudMusic\src\index.js
+ * @
+ */
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
