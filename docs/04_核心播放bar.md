cab889c7a1aa5d430040b991ccf08e6615bb7fc4
yayxs <leaderywl@163.com>
2020-09-01 23:15:11 +0800

    feat(comp:complete the bottom playback control bar): comp:Complete the bottom playback control bar
    n
    
    BREAKING CHANGE: no breaking changes

--- docs/04_核心播放bar.md
+++ docs/04_核心播放bar.md
@@ -1,7 +1,7 @@
 <!--
  * @Author: yayxs
  * @Date: 2020-08-31 23:17:19
- * @LastEditTime: 2020-08-31 23:17:45
+ * @LastEditTime: 2020-09-01 21:03:59
  * @LastEditors: yayxs
  * @Description:
  * @FilePath: \NeteaseCloudMusic\docs\04_核心播放bar.md
@@ -11,3 +11,5 @@
 ### 精灵图篇
 
 - [https://s2.music.126.net/style/web2/img/frame/playbar.png?4b285455950eeb8c77bec37cb2a2829c](https://s2.music.126.net/style/web2/img/frame/playbar.png?4b285455950eeb8c77bec37cb2a2829c)
+
+- [底部播放 bar](https://s2.music.126.net/style/web2/img/frame/statbar.png?fe9bf6f2a8cee166bf1b2ea588744650)
--- src/pages/player/playControlBar/index.jsx
+++ src/pages/player/playControlBar/index.jsx
@@ -1,17 +1,69 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
+import { Slider } from "antd";
 import "./index.scss";
 export default memo(function PlayerBar() {
+  const [defaultValue, setDefaultValue] = useState(30);
   return (
     <div className="play_container">
+      <section className="lock">
+        <a href="" className="btn"></a>
+      </section>
       <section className="wrap_980_center content ">
         <section className="btns">
-          <button className="pre btn"></button>
+          <a href="" className="pre btn"></a>
 
-          <button className="play btn"></button>
+          <a href="" className="play btn"></a>
 
-          <button className="next btn"></button>
+          <a href="" className="next btn"></a>
         </section>
-        <section className=""></section>
+        <section className="space"></section>
+        <section className="song_cover">
+          <img
+            src="https://p2.music.126.net/ZXByCLzRjyIQTyTCGXS3zg==/109951165277008551.jpg?param=34y34"
+            alt="cover"
+          />
+          <a href="" hidefocus="true" className="mask"></a>
+        </section>
+        <section className="play">
+          <section
+            className="j-flag words"
+            style={{ height: "28px", lineHeight: "28px" }}
+          >
+            <a
+              href=""
+              hidefocus="true"
+              className="f-thide name fc1 f-fl"
+              title=""
+              style={{ color: "#fff" }}
+            >
+              分手跳跃
+            </a>
+            <span className="by f-thide f-fl" style={{ marginLeft: "15px" }}>
+              <span title="隔壁老樊">
+                <a href="" hidefocus="true" title="" className="singer">
+                  隔壁老樊
+                </a>
+              </span>
+            </span>
+          </section>
+          <section className="pbar">
+            <Slider defaultValue={defaultValue} />
+            <span className="j-flag time">
+              <span>03:14</span> / 04:10
+            </span>
+          </section>
+        </section>
+        <section className="warp_con ">
+          <div className="left">
+            <button className="sprite_playbar btn favor"></button>
+            <button className="sprite_playbar btn share"></button>
+          </div>
+          <section className="right">
+            <button className="sprite_playbar btn volume"></button>
+            <button className="sprite_playbar btn loop"></button>
+            <button className="sprite_playbar btn playlist">112</button>
+          </section>
+        </section>
       </section>
     </div>
   );
